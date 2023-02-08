0624e172499c2fb8aeda03529a9d82dbb25874f7
Ken Hamamoto <k.hamamoto@fukuokabank.co.jp>
2023-02-08 18:33:21 +0900

    docker

--- src/main/resources/db/migration/V0_0_1__create_users.sql
+++ src/main/resources/db/migration/V0_0_1__create_users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE accounts
 (
     id         INT AUTO_INCREMENT                                              NOT NULL PRIMARY KEY,
+    user_id    INT AUTO_INCREMENT                                              NOT NULL,
     name       VARCHAR(30)                                                     NOT NULL,
     email      VARCHAR(30)                                                     NOT NULL,
     password   VARCHAR(30)                                                     NOT NULL,
@@ -10,13 +11,13 @@ CREATE TABLE accounts
 
 INSERT
 accounts (name, email, password)
-VALUES ('田中太郎', 'akamatsufumihito@example.co.jp', 'test'),
-        ('鈴木一郎', 'ito_523@example.com', 'test'),
-        ('松浦 多栄子', 'nishida119@example.org', 'test'),
-        ('堀田 隆次', 'yamashita_tetsuya@example.net', 'test'),
-        ('田辺 雅生', 'kosugi_takehiro@example.com', 'test'),
-        ('谷本 恵治', 'ishikawa_89@example.jp', 'test'),
-        ('鶴田 光範', 'junya_kondou@example.org', 'test'),
-        ('片山 有', 'kento_kanou@example.org', 'test'),
-        ('島崎 直秋', 'kinoshita1017@example.ne.jp', 'test'),
-        ('大沢 祐美', 'ito1117@example.net', 'test');
+VALUES  ('tanaka', 'akamatsufumihito@example.co.jp', 'test'),
+        ('suzuki', 'ito_523@example.com', 'test'),
+        ('matsuura', 'nishida119@example.org', 'test'),
+        ('hotta', 'yamashita_tetsuya@example.net', 'test'),
+        ('tanabe', 'kosugi_takehiro@example.com', 'test'),
+        ('tanimoto', 'ishikawa_89@example.jp', 'test'),
+        ('tsuruta', 'junya_kondou@example.org', 'test'),
+        ('katayama', 'kento_kanou@example.org', 'test'),
+        ('simazaki', 'kinoshita1017@example.ne.jp', 'test'),
+        ('oosawa', 'ito1117@example.net', 'test');
