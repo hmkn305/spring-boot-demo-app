e02c44f05a79d45d621b753fa1371ba799da0d8d
Ken Hamamoto <k.hamamoto@fukuokabank.co.jp>
2023-02-09 09:15:13 +0900

    dockerでmysqlに接続 migrationがうまくいかないので一旦放置して手動でmysqlにデータを挿入していく

--- src/main/resources/db/migration/V0_0_1__create_users.sql
+++ src/main/resources/db/migration/V0_0_1__create_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE accounts
 (
     id         INT AUTO_INCREMENT                                              NOT NULL PRIMARY KEY,
-    user_id    INT AUTO_INCREMENT                                              NOT NULL,
+    user_id    INT                                                             NOT NULL,
     name       VARCHAR(30)                                                     NOT NULL,
     email      VARCHAR(30)                                                     NOT NULL,
     password   VARCHAR(30)                                                     NOT NULL,
@@ -10,14 +10,14 @@ CREATE TABLE accounts
 );
 
 INSERT
-accounts (name, email, password)
-VALUES  ('tanaka', 'akamatsufumihito@example.co.jp', 'test'),
-        ('suzuki', 'ito_523@example.com', 'test'),
-        ('matsuura', 'nishida119@example.org', 'test'),
-        ('hotta', 'yamashita_tetsuya@example.net', 'test'),
-        ('tanabe', 'kosugi_takehiro@example.com', 'test'),
-        ('tanimoto', 'ishikawa_89@example.jp', 'test'),
-        ('tsuruta', 'junya_kondou@example.org', 'test'),
-        ('katayama', 'kento_kanou@example.org', 'test'),
-        ('simazaki', 'kinoshita1017@example.ne.jp', 'test'),
-        ('oosawa', 'ito1117@example.net', 'test');
+accounts (user_id, name, email, password)
+VALUES  (1, 'tanaka', 'akamatsufumihito@example.co.jp', 'test'),
+        (2, 'suzuki', 'ito_523@example.com', 'test'),
+        (3, 'matsuura', 'nishida119@example.org', 'test'),
+        (4, 'hotta', 'yamashita_tetsuya@example.net', 'test'),
+        (5, 'tanabe', 'kosugi_takehiro@example.com', 'test'),
+        (6, 'tanimoto', 'ishikawa_89@example.jp', 'test'),
+        (7, 'tsuruta', 'junya_kondou@example.org', 'test'),
+        (8, 'katayama', 'kento_kanou@example.org', 'test'),
+        (9, 'simazaki', 'kinoshita1017@example.ne.jp', 'test'),
+        (10, 'oosawa', 'ito1117@example.net', 'test');
